c6697cdf75efbeda6e405aef0a2fab9ae39499d0
GOOSLI <astan2211@yandex.ru>
2020-09-03 18:44:08 +0300

    Tank3
    Tank3

---
+++ Space-Mace/Assets/Component.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public abstract class Component : MonoBehaviour
+    {
+        public int mass;
+        public TileBase texture;
+
+        public abstract void TileSet(Vector3Int clickCellPosition);
+    }
