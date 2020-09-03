6a1d400da47b697d61be35f11c64d7a8d5ceefa0
GOOSLI <astan2211@yandex.ru>
2020-09-03 18:23:04 +0300

    Tank+Wall
    added Tank and Wall

---
+++ Space-Mace/Assets/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    public class Wall : Component
+    {
+
+        public Wall()
+            {
+                mass = 1;
+                
+            }
+
+
+       public override void TileSet(Vector3Int clickCellPosition)
+       {
+           Ship.map.SetTile(clickCellPosition, texture);
+       }
+    }
